c1cc331c7c0c1cb2fbca3a3c9bea43446a59ba6c
Morgan <morgan.moreno25@gmail.com>
2021-03-10 17:27:55 -0600

    [App.js]
    - Added a container to act as a root component for all content
    
    - Added a route for the RegisterForm
    
    [Header.js]
    
    - Removed color declarations and added them to App.scss file
    
    -  Changed the variant of the title text from h2 to h3 and added an id to it to make styling simpler
    
    - Added id's to each button to make styling simpler

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { useEffect } from 'react';
-import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Ledger from './components/Ledger';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginForm from './components/auth/LoginForm';
+
+import { Container } from '@material-ui/core';
+
+import './App.scss';
+import RegisterForm from './components/auth/RegisterForm';
 
 export default function App() {
 	return (
 		<Router>
 			<div className='App' id='app'>
 				<Header />
-				<Switch>
-					<Route path='/' exact></Route>
-					<Route path='/login'>
-						<LoginForm />
-					</Route>
-					<Route path='/app'>
-						<Nav />
-						<Ledger />
-					</Route>
-				</Switch>
+				<Container id='content' maxWidth='xl'>
+					<Switch>
+						<Route path='/' exact></Route>
+						<Route path='/login'>
+							<LoginForm />
+						</Route>
+						<Route path='/register'>
+							<RegisterForm />
+						</Route>
+						<Route path='/app'>
+							<Nav />
+							<Ledger />
+						</Route>
+					</Switch>
+				</Container>
 			</div>
 		</Router>
 	);
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,19 +9,20 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 	const title = useSelector(state => state.app.title);
-	const github = useSelector(state => state.app.githubLink);
 
 	return (
 		<AppBar id='main-header'>
 			<Icon id='logo'>
 				<MonetizationIcon fontSize='large'></MonetizationIcon>
 			</Icon>
-			<Typography variant='h3'>{title}</Typography>
+			<Typography variant='h2' id='header-title'>
+				{title}
+			</Typography>
 			<div id='header-buttons'>
-				<Link component={Button} to='/login' color='primary' variant='contained'>
+				<Link component={Button} to='/login' variant='contained' id='login-button'>
 					Login
 				</Link>
-				<Link component={Button} color='secondary' variant='contained' to='/register'>
+				<Link component={Button} variant='contained' to='/register' id='register-button'>
 					Register
 				</Link>
 			</div>
