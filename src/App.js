76aab8c0234d6e48dc77b34b4eead9b581dab022
Morgan <morgan.moreno25@gmail.com>
2021-03-10 16:14:45 -0600

    Added routing to seperate Landing Page, Auth, and App sections

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Ledger from './components/Ledger';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
 	return (
 		<Router>
 			<div className='App' id='app'>
 				<Header />
-				<Nav />
-				<Ledger />
+				<Switch>
+					<Route path='/' exact></Route>
+					<Route path='/auth'></Route>
+					<Route path='/app'>
+						<Nav />
+						<Ledger />
+					</Route>
+				</Switch>
 			</div>
 		</Router>
 	);
