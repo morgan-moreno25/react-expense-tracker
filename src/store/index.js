a21f499a948458b10e155ebaaf26d9525802e0a4
Morgan <morgan.moreno25@gmail.com>
2021-03-10 15:30:46 -0600

    Fixed error 'configurestore' is not exported from '@reduxjs/toolkit'

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import { configurestore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import appReducer from './slices/app.slice';
 import authReducer from './slices/auth.slice';
 import expenseReducer from './slices/expense.slice';
 import incomeReducer from './slices/income.slice';
 
-export default configurestore({
+export default configureStore({
 	reducer: {
 		app: appReducer,
 		auth: authReducer,
