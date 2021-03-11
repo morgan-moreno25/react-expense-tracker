e6968c6ec812a3c5c03d3fa0be20c1e9a882e76e
Morgan <morgan.moreno25@gmail.com>
2021-03-11 11:11:50 -0600

    Removed the root element of the ledger component.
    Decided to just go with a switch component here and handle any layout adjustments within the individual content components themselves

--- src/components/app/Ledger.js
+++ src/components/app/Ledger.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import { Box } from '@material-ui/core';
-import BalanceDisplay from './BalanceDisplay';
+import Dashboard from './Dashboard';
 import IncomeTable from './IncomeTable';
 import ExpenseTable from './ExpenseTable';
 
 export default function Ledger() {
 	return (
-		<Box id='ledger'>
-			<BalanceDisplay />
-			<IncomeTable />
-			<ExpenseTable />
-		</Box>
+		<Switch>
+			<Route path='/app'>
+				<Dashboard />
+			</Route>
+			<Route path='/app/income'>
+				<IncomeTable />
+			</Route>
+			<Route path='/app/expenses'>
+				<ExpenseTable />
+			</Route>
+		</Switch>
 	);
 }
