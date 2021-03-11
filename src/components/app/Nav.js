ad807a1602d3b5bca9b2852479300065cc7b4ca6
Morgan <morgan.moreno25@gmail.com>
2021-03-11 09:58:08 -0600

    Updated the component to represent an actual navigation bar
    Added links for the following:
    - Dashboard
    - Income
    - Expenses

--- src/components/app/Nav.js
+++ src/components/app/Nav.js
@@ -1,53 +1,44 @@
 import React, { useState } from 'react';
 
-import Container from '@material-ui/core/Container';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
+import { Container, Button, Paper, List, ListItem, ListItemIcon } from '@material-ui/core';
+import { DashboardSharp, AttachMoneySharp, ExitToAppSharp } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import Form from './Form';
 
+/**
+ * @TODO
+ *
+ * Update this component so that it represents an actual navigation bar.
+ * The navigation options should be:
+ * - Dashboard
+ * - Income
+ * - Expenses
+ */
+
 export default function Nav() {
-	const [formType, setFormType] = useState('');
-
-	const toggleForm = e => {
-		if (
-			e.target.parentElement.id === 'show-income-form' ||
-			e.target.id === 'show-income-form'
-		) {
-			setFormType('income');
-		}
-		if (
-			e.target.parentElement.id === 'show-expense-form' ||
-			e.target.id === 'show-expense-form'
-		) {
-			setFormType('expense');
-		}
-	};
-
 	return (
 		<Container id='nav'>
-			<nav>
-				<section id='form-controls'>
-					<ButtonGroup
-						variant='contained'
-						color='inherit'
-						aria-label='text primary button group'
-					>
-						<Button id='show-income-form' onClick={toggleForm}>
-							Income
-						</Button>
-						<Button id='show-expense-form' onClick={toggleForm}>
-							Expense
-						</Button>
-					</ButtonGroup>
-				</section>
-				<section id='form-section'>
-					<Paper elevation={3} variant='outlined' square id='form-container'>
-						<Form formType={formType} />
-					</Paper>
-				</section>
-			</nav>
+			<List component='nav'>
+				<ListItem button>
+					<ListItemIcon>
+						<DashboardSharp />
+					</ListItemIcon>
+					<Link to='/app/dashboard'>Dashboard</Link>
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<AttachMoneySharp />
+					</ListItemIcon>
+					<Link to='/app/income'>Income</Link>
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<ExitToAppSharp />
+					</ListItemIcon>
+					<Link to='/app/expenses'>Expenses</Link>
+				</ListItem>
+			</List>
 		</Container>
 	);
 }
