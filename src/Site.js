0974e2589f87d53924ec63c984114db20eaaf0d3
Morgan <morgan.moreno25@gmail.com>
2021-03-10 18:50:20 -0600

    Changed the name of App.js to Site.js

--- src/Site.js
+++ src/Site.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUser } from './store/slices/auth.slice';
 
 import Header from './components/Header';
 import Nav from './components/Nav';
@@ -12,6 +14,18 @@ import './App.scss';
 import RegisterForm from './components/auth/RegisterForm';
 
 export default function App() {
+	const dispatch = useDispatch();
+
+	const authenticated = useSelector(state => state.auth.authenticated);
+
+	useEffect(() => {
+		dispatch(loadUser());
+	}, [dispatch]);
+
+	if (authenticated) {
+		return <Redirect to='/app' />;
+	}
+
 	return (
 		<Router>
 			<div className='App' id='app'>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Site from './Site';
 import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<Site />
 	</Provider>,
 	document.getElementById('root')
 );
