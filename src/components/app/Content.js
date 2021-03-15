8d6dcbe32fbb171886162c026a6c04ddcc691d8c
Morgan <morgan.moreno25@gmail.com>
2021-03-15 11:18:27 -0500

    Set up the Income page
    Added modals for both adding/editing income items
    
    Set up a table to display all income items
    
    Set up add, delete, and update functionality for each income object
    
    # TODO:
    
    Add filters so that the user can sort the data as they please

--- src/components/app/Content.js
+++ src/components/app/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
-import IncomeTable from './IncomeTable';
+import Income from './income';
 import ExpenseTable from './ExpenseTable';
 
 export default function Content() {
@@ -12,7 +12,7 @@ export default function Content() {
 				<Dashboard />
 			</Route>
 			<Route path='/app/income'>
-				<IncomeTable />
+				<Income />
 			</Route>
 			<Route path='/app/expenses'>
 				<ExpenseTable />
---
+++ src/components/app/income/EditIncomeModal.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateIncome } from '../../../store/slices/income.slice';
+
+import {
+	Container,
+	Button,
+	Modal,
+	TextField,
+	FormControl,
+	InputLabel,
+	Input,
+	Paper,
+	Typography,
+} from '@material-ui/core';
+
+export default function EditIncomeModal({ open, toggle, income }) {
+	const dispatch = useDispatch();
+
+	const [date, setDate] = useState('');
+	const [category, setCategory] = useState('');
+	const [amount, setAmount] = useState('');
+
+	useEffect(() => {
+		setDate(income.date ? income.date.date : '');
+		setCategory(income.category ? income.category : '');
+		setAmount(income.amount ? income.amount : '');
+	}, [income]);
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+
+		const incomeDetails = { date, category, amount };
+
+		const resultAction = await dispatch(updateIncome({ id: income.id, income: incomeDetails }));
+		if (updateIncome.fulfilled.match(resultAction)) {
+			console.log(resultAction.payload);
+
+			handleClose();
+		} else {
+			console.log(resultAction.payload);
+		}
+	};
+
+	const handleClose = () => {
+		setDate('');
+		setCategory('');
+		setAmount('');
+
+		toggle();
+	};
+
+	return (
+		<Modal open={open} onClose={handleClose} id='income-edit-modal'>
+			<Paper component='form' elevation={10} id='income-edit-form'>
+				<Typography variant='h3'>Enter Income Details</Typography>
+				<TextField
+					id='date'
+					name='date'
+					type='date'
+					value={date}
+					onChange={e => setDate(e.target.value)}
+				/>
+				<FormControl>
+					<InputLabel htmlFor='category'>Category</InputLabel>
+					<Input
+						type='text'
+						id='category'
+						name='category'
+						value={category}
+						onChange={e => setCategory(e.target.value)}
+					/>
+				</FormControl>
+				<FormControl>
+					<InputLabel htmlFor='amount'>Amount</InputLabel>
+					<Input
+						type='number'
+						id='amount'
+						name='amount'
+						value={amount}
+						onChange={e => setAmount(Number(e.target.value))}
+					/>
+				</FormControl>
+				<div id='income-edit-form-buttons'>
+					<Button variant='outlined' id='income-edit-submit' onClick={handleSubmit}>
+						Submit
+					</Button>
+					<Button variant='outlined' id='income-edit-cancel' onClick={handleClose}>
+						Cancel
+					</Button>
+				</div>
+			</Paper>
+		</Modal>
+	);
+}
