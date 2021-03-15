ff04c2911873be3ff5f598ae73e79d569cf4bb83
Morgan <morgan.moreno25@gmail.com>
2021-03-15 11:15:27 -0500

    Update to the main application component
    Added a useEffect hook to update the income and expense items in state on load
    
    Changed the Ledger component to the Content component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllIncome } from '../../store/slices/income.slice';
+import { getAllExpenses } from '../../store/slices/expense.slice';
 
 import { Container } from '@material-ui/core';
 import Nav from './Nav';
-import Ledger from './Ledger';
+import Content from './Content';
 
 export default function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getAllIncome());
+		dispatch(getAllExpenses());
+	}, [dispatch]);
+
 	return (
 		<Container id='content' maxWidth='xl'>
 			<Nav />
-			<Ledger />
+			<Content />
 		</Container>
 	);
 }
