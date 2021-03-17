41570f5e44ddf255927cb9242b003c8e64471ab0
Morgan <morgan.moreno25@gmail.com>
2021-03-17 17:43:50 -0500

    Updated Dashboard
    Fixed annoying floating point precision
    
    Moved the netBalance card to the end so that the display follows the logic of calculations

--- src/components/app/Dashboard.js
+++ src/components/app/Dashboard.js
@@ -7,23 +7,19 @@ export default function Dashboard() {
 	const expenses = useSelector(state => state.expense.data);
 	const income = useSelector(state => state.income.data);
 
-	const totalExpense = expenses.reduce((prev, cur) => prev + Number(cur.amount), 0);
-	const totalIncome = income.reduce((prev, cur) => prev + Number(cur.amount), 0);
-	const netBalance = totalIncome - totalExpense;
+	const totalExpense = Math.fround(
+		expenses.reduce((prev, cur) => prev + Number(cur.amount), 0)
+	).toFixed(2);
+	const totalIncome = Math.fround(
+		income.reduce((prev, cur) => prev + Number(cur.amount), 0)
+	).toFixed(2);
+	const netBalance = Math.fround(totalIncome - totalExpense).toFixed(2);
 	const title = netBalance >= 0 ? 'Net Income' : 'Net Loss';
 	const balanceClass = netBalance >= 0 ? 'gain' : 'loss';
 
 	return (
 		<Box id='dashboard'>
 			<Container id='dashboard-balances'>
-				<Card component={Paper} elevation={10} variant='outlined'>
-					<CardContent>
-						<Typography variant='h4'>{title}</Typography>
-						<Typography variant='h4' className={balanceClass}>
-							${netBalance}
-						</Typography>
-					</CardContent>
-				</Card>
 				<Card component={Paper} elevation={10} variant='outlined'>
 					<CardContent>
 						<Typography variant='h4'>Total Income</Typography>
@@ -34,6 +30,14 @@ export default function Dashboard() {
 					<CardContent>
 						<Typography variant='h4'>Total Expenses</Typography>
 						<Typography variant='h4'>${totalExpense}</Typography>
+					</CardContent>
+				</Card>
+				<Card component={Paper} elevation={10} variant='outlined'>
+					<CardContent>
+						<Typography variant='h4'>{title}</Typography>
+						<Typography variant='h4' className={balanceClass}>
+							${netBalance}
+						</Typography>
 					</CardContent>
 				</Card>
 			</Container>
